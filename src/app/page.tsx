fd09aac353c6c6cb7e70c3d7a38bd6d745673ece
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-08-02 11:21:11 -0300

    format code with biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
 	return (
@@ -6,5 +6,5 @@ export default function Home() {
 			<p>Carrinho Venda Rápida</p>
 			<Button>Teste</Button>
 		</div>
-	);
+	)
 }
